9e3f179408d37840f17398dbee4f8d3d7fb2f204
Kai Krings <kai.krings@posteo.de>
2023-12-30 15:33:02 +0000

    sensor: Testing lint-staged configuration

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,3 @@
-import path from 'path';
-
 export default {
-  '**/*.{py,ts,tsx}': (files) => {
-    const cwd = process.cwd();
-    files = files.map((file) => path.relative(cwd, file));
-    return `nx affected -t format --files=${files.join(',')}`;
-  }
+  '**/*.{py,ts,tsx}': (files) => `nx affected -t format --files=${files.join(',')}`
 }
