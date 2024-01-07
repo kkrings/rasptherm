72b08911ec46388f1da338e24ee779578f59574b
Kai Krings <kai.krings@posteo.de>
2024-01-07 11:50:47 +0000

    sensor: Adding CLI command for serving sensor with TLS

--- packages/sensor/sensor/cli/serve.py
+++ packages/sensor/sensor/cli/serve.py
@@ -1,5 +1,24 @@
-from sensor.setup import non_ssl_sensor_setup
+from sensor.setup import non_ssl_sensor_setup, ssl_sensor_setup
 from sensor.types import ReadSensor
+
+
+async def serve(
+    port: int,
+    variant: ReadSensor,
+    ssl_certificate: str,
+    ssl_private_key: str,
+    ssl_root_certificate: str | None = None,
+) -> None:
+    sensor, _ = ssl_sensor_setup(
+        read_sensor=variant,
+        ssl_certificate=ssl_certificate,
+        ssl_private_key=ssl_private_key,
+        ssl_root_certificate=ssl_root_certificate,
+        port=port,
+    )
+
+    await sensor.start()
+    await sensor.wait_for_termination()
 
 
 async def serve_witout_ssl(port: int, variant: ReadSensor) -> None:
--- packages/sensor/sensor/setup.py
+++ packages/sensor/sensor/setup.py
@@ -1,3 +1,4 @@
+import grpc  # type: ignore
 import grpc.aio  # type: ignore
 
 from sensor.interface.sensor_pb2_grpc import add_SensorServicer_to_server
@@ -5,10 +6,31 @@ from sensor.sensor import Sensor
 from sensor.types import ReadSensor, SensorSetup
 
 
+def ssl_sensor_setup(
+    read_sensor: ReadSensor,
+    ssl_certificate: str,
+    ssl_private_key: str,
+    ssl_root_certificate: str | None = None,
+    port: int = 0,
+) -> SensorSetup:
+    sensor = _create_sensor(read_sensor)
+
+    server_credentials = grpc.ssl_server_credentials(
+        private_key_certificate_chain_pairs=[(ssl_private_key, ssl_certificate)],
+        root_certificates=ssl_root_certificate,
+        require_client_auth=True if ssl_root_certificate is not None else False,
+    )
+
+    port = sensor.add_secure_port(
+        address=f"[::]:{port}", server_credentials=server_credentials
+    )
+
+    return SensorSetup(sensor, port)
+
+
 def non_ssl_sensor_setup(read_sensor: ReadSensor, port: int = 0) -> SensorSetup:
     sensor = _create_sensor(read_sensor)
-    port = sensor.add_insecure_port(f"[::]:{port}")
-
+    port = sensor.add_insecure_port(address=f"[::]:{port}")
     return SensorSetup(sensor, port)
 
 
