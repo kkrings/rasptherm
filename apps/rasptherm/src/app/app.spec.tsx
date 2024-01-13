9d3d59beaf0b19341161dd7cdf9ac89de02126b2
Kai Krings <kai.krings@posteo.de>
2024-01-13 18:19:34 +0000

    Minor change in project configurations

--- apps/rasptherm/src/app/app.spec.tsx
+++ apps/rasptherm/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome rasptherm/gi)).toBeTruthy();
+    expect(getByText(/Welcome rasptherm/i)).toBeTruthy();
   });
 });
