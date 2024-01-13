a0b3a22c040673d07b606f40ff53f093a0597d8b
Kai Krings <kai.krings@posteo.de>
2024-01-13 18:29:33 +0000

    Removig fix, write, and check configurations

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,8 +3,8 @@ export default {
     const files = fileList.join(',');
     return [
       `nx format:write --files=${files}`,
-      `nx affected -t format -c write --files=${files}`,
-      `nx affected -t lint -c fix --files=${files}`
+      `nx affected -t format --files=${files}`,
+      `nx affected -t lint --fix --files=${files}`
     ];
   }
 }
