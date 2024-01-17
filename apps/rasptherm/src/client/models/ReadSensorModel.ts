c38ac599524b70a98fa693b90e2a60b949a8c3fd
Kai Krings <kai.krings@posteo.de>
2024-01-17 19:26:08 +0000

    rasptherm: Generating API client

---
+++ apps/rasptherm/src/client/models/ReadSensorModel.ts
@@ -0,0 +1,9 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type ReadSensorModel = {
+    temperature_degree_celsius: number;
+    relative_humidity_percent: number;
+};
+
