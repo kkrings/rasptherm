280d3717555879278dd1d25096945d3750bfa316
Kai Krings <kai.krings@posteo.de>
2024-01-17 19:26:51 +0000

    rasptherm-backend: Enabling CORS

--- apps/rasptherm-backend/rasptherm_backend/main.py
+++ apps/rasptherm-backend/rasptherm_backend/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from rasptherm_backend.models.metadata import MetadataModel
 from rasptherm_backend.routers.sensor import sensor_router
 from rasptherm_backend.services.metadata import get_metadata
 
 app = FastAPI()
+
+origins = ["http://localhost:4200"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
