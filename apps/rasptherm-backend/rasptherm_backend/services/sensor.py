c17cd85396b6aea4c6d1bb8837d233fd941215a9
Kai Krings <kai.krings@posteo.de>
2024-02-23 13:44:33 +0000

    backend: Adding sensor connection with SSL

--- apps/rasptherm-backend/rasptherm_backend/services/sensor.py
+++ apps/rasptherm-backend/rasptherm_backend/services/sensor.py
@@ -1,8 +1,10 @@
-from collections.abc import AsyncIterator
+from collections.abc import AsyncIterator, Callable
+from contextlib import AbstractAsyncContextManager, asynccontextmanager
+from pathlib import Path
 from typing import Annotated, Protocol
 
 from fastapi import Depends
-from rasptherm_sensor.client import non_ssl_sensor_connection
+from rasptherm_sensor.client import non_ssl_sensor_connection, ssl_sensor_connection
 from rasptherm_sensor.types import SensorReadout
 
 from rasptherm_backend.models.sensor import ReadSensorModel
@@ -14,11 +16,82 @@ class Sensor(Protocol):
         ...
 
 
-async def get_sensor(
+SensorConnection = Callable[[], AbstractAsyncContextManager[Sensor]]
+
+
+class SslSensorConnection:
+    def __init__(
+        self,
+        host: str,
+        sensor_ssl_root_certificate: str,
+        sensor_ssl_private_key: str | None = None,
+        sensor_ssl_certificate: str | None = None,
+    ) -> None:
+        self._host = host
+        self._sensor_ssl_root_certificate = sensor_ssl_root_certificate
+        self._sensor_ssl_private_key = sensor_ssl_private_key
+        self._sensor_ssl_certificate = sensor_ssl_certificate
+
+    @asynccontextmanager
+    async def __call__(self) -> AsyncIterator[Sensor]:
+        sensor_connection = ssl_sensor_connection(
+            host=self._host,
+            ssl_root_certificate=self._sensor_ssl_root_certificate,
+            ssl_private_key=self._sensor_ssl_private_key,
+            ssl_certificate=self._sensor_ssl_certificate,
+        )
+
+        async with sensor_connection as sensor:
+            yield sensor
+
+
+class NonSslSensorConnection:
+    def __init__(self, host: str) -> None:
+        self._host = host
+
+    @asynccontextmanager
+    async def __call__(self) -> AsyncIterator[Sensor]:
+        async with non_ssl_sensor_connection(host=self._host) as sensor:
+            yield sensor
+
+
+def get_sensor_connection(
     settings: Annotated[Settings, Depends(get_settings)],
+) -> SensorConnection:
+    host = str(settings.sensor_host)
+
+    if settings.sensor_ssl_root_certificate is None:
+        return NonSslSensorConnection(host)
+
+    sensor_ssl_root_certificate = _read_file(
+        filepath=settings.sensor_ssl_root_certificate
+    )
+
+    sensor_client_ssl_private_key = (
+        _read_file(filepath=settings.sensor_ssl_private_key_and_certificate[0])
+        if settings.sensor_ssl_private_key_and_certificate is not None
+        else None
+    )
+
+    sensor_client_ssl_certificate = (
+        _read_file(filepath=settings.sensor_ssl_private_key_and_certificate[1])
+        if settings.sensor_ssl_private_key_and_certificate is not None
+        else None
+    )
+
+    return SslSensorConnection(
+        host,
+        sensor_ssl_root_certificate,
+        sensor_client_ssl_private_key,
+        sensor_client_ssl_certificate,
+    )
+
+
+async def get_sensor(
+    sensor_connection: Annotated[SensorConnection, get_sensor_connection],
 ) -> AsyncIterator[Sensor]:
-    async with non_ssl_sensor_connection(host=str(settings.sensor_host)) as client:
-        yield client
+    async with sensor_connection() as sensor:
+        yield sensor
 
 
 async def read_sensor(sensor: Sensor) -> ReadSensorModel:
@@ -29,3 +102,10 @@ async def read_sensor(sensor: Sensor) -> ReadSensorModel:
         relative_humidity_percent=readout.relative_humidity_percent,
         executed_at_utc=readout.executed_at_utc,
     )
+
+
+def _read_file(filepath: Path) -> str:
+    with open(filepath) as filestream:
+        filecontent = filestream.read()
+
+    return filecontent
--- apps/rasptherm-backend/rasptherm_backend/services/settings.py
+++ apps/rasptherm-backend/rasptherm_backend/services/settings.py
@@ -1,11 +1,13 @@
 from functools import lru_cache
 
-from pydantic import AnyHttpUrl, AnyUrl
+from pydantic import AnyHttpUrl, AnyUrl, FilePath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     sensor_host: AnyUrl
+    sensor_ssl_root_certificate: FilePath | None = None
+    sensor_ssl_private_key_and_certificate: tuple[FilePath, FilePath] | None = None
     cors_enabled: bool = False
     cors_allow_origins: set[AnyHttpUrl] = set()
     model_config = SettingsConfigDict(env_prefix="backend_")
