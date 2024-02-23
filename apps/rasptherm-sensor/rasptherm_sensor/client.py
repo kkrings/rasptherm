841d6a500a42e6fad7781e3863b2894a4c6b6bf4
Kai Krings <kai.krings@posteo.de>
2024-02-23 13:46:14 +0000

    sensor: Adding sensor connection with SSL

--- apps/rasptherm-sensor/rasptherm_sensor/client.py
+++ apps/rasptherm-sensor/rasptherm_sensor/client.py
@@ -1,7 +1,9 @@
 from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 from datetime import timezone
+from typing import Optional
 
+import grpc  # type: ignore
 import grpc.aio  # type: ignore
 
 from rasptherm_sensor.interface.sensor_pb2 import ReadSensorRequest, ReadSensorResponse
@@ -23,6 +25,26 @@ class SensorClient:
             relative_humidity_percent=response.relative_humidity_percent,
             executed_at_utc=response.executed_at.ToDatetime(timezone.utc),
         )
+
+
+@asynccontextmanager
+async def ssl_sensor_connection(
+    host: str,
+    ssl_root_certificate: str,
+    ssl_private_key: Optional[str] = None,
+    ssl_certificate: Optional[str] = None,
+) -> AsyncIterator[SensorClient]:
+    channel_credentials = grpc.ssl_channel_credentials(
+        root_certificates=ssl_root_certificate,
+        private_key=ssl_private_key,
+        certificate_chain=ssl_certificate,
+    )
+
+    async with grpc.aio.secure_channel(
+        target=host, credentials=channel_credentials
+    ) as channel:
+        sensor = SensorStub(channel)  # type: ignore
+        yield SensorClient(sensor)
 
 
 @asynccontextmanager
