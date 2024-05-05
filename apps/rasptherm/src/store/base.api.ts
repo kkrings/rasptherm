2651de05eada17e12e1a184c565154a3253b0c42
Kai Krings <kai.krings@posteo.de>
2024-05-05 18:41:26 +0000

    frontent: Simplifying base query code

--- apps/rasptherm/src/store/base.api.ts
+++ apps/rasptherm/src/store/base.api.ts
@@ -1,19 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '/' });
-
-const getBaseQuery: typeof baseQuery = (args, api, extraOptions) => {
-  const baseUrl = import.meta.env.VITE_BACKEND_URL;
-
-  const modifiedArgs =
-    typeof args === 'string'
-      ? `${baseUrl}${args}`
-      : { ...args, url: `${baseUrl}${args.url}` };
-
-  return baseQuery(modifiedArgs, api, extraOptions);
-};
+const baseQuery: ReturnType<typeof fetchBaseQuery> = (
+  args,
+  api,
+  extraOptions
+) =>
+  fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKEND_URL })(
+    args,
+    api,
+    extraOptions
+  );
 
 export const baseApi = createApi({
-  baseQuery: getBaseQuery,
+  baseQuery,
   endpoints: () => ({}),
 });
